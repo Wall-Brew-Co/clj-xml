f5d36b1539863e4671bd122fe6af3a4c001178b7
nnichols <nichols1991@gmail.com>
2021-03-05 15:04:56 +0000

    Bump org.clojure/clojure from 1.10.2 to 1.10.3

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/nnichols/clj-xml"
   :license {:name "MIT"
             :url  "https://opensource.org/licenses/MIT"}
-  :dependencies [[org.clojure/clojure "1.10.2"]
+  :dependencies [[org.clojure/clojure "1.10.3"]
                  [org.clojure/data.xml "0.2.0-alpha6"]]
   :profiles {:uberjar {:aot :all}}
   :min-lein-version "2.5.3")
