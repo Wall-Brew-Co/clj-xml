f5e2be85c2cb6ae25191f5669ad1fa05cf7051ef
Nick A Nichols <nichols1991@gmail.com>
2022-12-10 08:41:31 -0600

    [WB-35]: Install deployment GitHub action (#45)
    * Add config to deploy to clojars with CI
    
    * Bump version

--- project.clj
+++ project.clj
@@ -1,9 +1,18 @@
-(defproject com.wallbrew/clj-xml "1.7.1"
+(defproject com.wallbrew/clj-xml "1.7.2"
   :description "The missing link between clj and xml"
   :url "https://github.com/nnichols/clj-xml"
-  :license {:name "MIT"
-            :url  "https://opensource.org/licenses/MIT"}
+  :license {:name         "MIT"
+            :url          "https://opensource.org/licenses/MIT"
+            :distribution :repo
+            :comments     "Same-as all Wall-Brew projects"}
+  :scm {:name "git"
+        :url  "https://github.com/Wall-Brew-Co/clj-xml"}
   :dependencies [[org.clojure/clojure "1.11.1"]
                  [org.clojure/data.xml "0.2.0-alpha8"]]
+  :plugins [[lein-project-version "0.1.0"]]
+  :deploy-repositories [["clojars" {:url           "https://clojars.org/repo"
+                                    :username      :env/clojars_user
+                                    :password      :env/clojars_pass
+                                    :sign-releases false}]]
   :profiles {:uberjar {:aot :all}}
   :min-lein-version "2.5.3")
