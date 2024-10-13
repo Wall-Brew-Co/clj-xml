e297111bc4eae7df19d75839b446a3ef458cff6b
Nick Nichols <nichols1991@gmail.com>
2024-10-13 21:25:26 +0000

    Clean up wallbrew config

--- project.clj
+++ project.clj
@@ -13,8 +13,8 @@
   :dependencies [[org.clojure/clojure "1.12.0"]
                  [org.clojure/data.xml "0.2.0-alpha9"]]
   :plugins [[com.github.clj-kondo/lein-clj-kondo "2024.09.27"]
-            [com.wallbrew/lein-sealog "1.7.0"]
-            [com.wallbrew/bouncer "1.0.0"]
+            [com.wallbrew/lein-sealog "1.8.0"]
+            [com.wallbrew/bouncer "1.1.1"]
             [mvxcvi/cljstyle "0.16.630"]]
   :deploy-branches ["master"]
   :deploy-repositories [["clojars" {:url           "https://clojars.org/repo"
